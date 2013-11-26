8ed82649c0e8afd1b0a100b155c3739d64f1f5a2
Gilmar Lira <gilmar.lira@me.com>
2013-11-26 13:28:07 -0800

    Create first working prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 /* **********************************************
-     Begin app.js
-********************************************** */;
+     Begin interaction.js
+********************************************** */// Interaction
+// ////////////////////////////////////////////////////
+function initContainer(){containerWidth=$(window).width();containerHeight=$(window).height();$(".container").css({height:containerHeight});console.log(containerHeight)}var containerWidth,containerHeight,delay=function(){var e=0;return function(t,n){clearTimeout(e);e=setTimeout(t,n)}}();$(window).resize(function(){delay(function(){console.log("Resize...");initContainer()},100)});$(document).ready(function(){$(".main_nav li").click(function(){var e=$(this).index();console.log(e);$(".content section").eq(e).show().siblings("section").hide()})});
