885bf037056c188be347a4c47e56bcbdf499123e
Gilmar Lira <gilmar.lira@me.com>
2013-12-04 11:33:07 -0800

    Implement snapping.
    - Now using jQuery panelSnap plugin to drive navigation (replacing the previous smothScroll plugin)
    
    - Major changes to CSS and JS
    
    - Replacing JS functionality with pure CSS alternatives (section height control)
    
    - Content layout change: 100px padding-top, 1-column gutter between nav and content and width reduced to 6 columns.

--- build/js/scripts/interaction.js
+++ build/js/scripts/interaction.js
@@ -4,63 +4,78 @@
 
 window.addEventListener('load', initContainer, false);
 
-var containerWidth, containerHeight, colorInput;
+// var containerWidth, containerHeight, colorInput;
 
-var delay = (function(){
-      var timer = 0;
-      return function(callback, ms){
-        clearTimeout (timer);
-        timer = setTimeout(callback, ms);
-      };
-    })();
+// var delay = (function(){
+//       var timer = 0;
+//       return function(callback, ms){
+//         clearTimeout (timer);
+//         timer = setTimeout(callback, ms);
+//       };
+//     })();
 
-$(window).resize(function() { 
-    delay(function(){
-            console.log('Resize...');
-            initContainer();
-        }, 100);
-});
+// $(window).resize(function() { 
+//     delay(function(){
+//             console.log('Resize...');
+//             initContainer();
+//         }, 250);
+// });
 
 
 // Get viewport dimensions
 function initContainer() {
+    // containerWidth = $(window).width();
+    // containerHeight = $(window).height();
+
+    // $(".content>section").css({
+    //     minHeight: containerHeight
+    // });
+    
+    // console.log(containerHeight);
+
     colorInput = document.getElementById('color-input');
     colorInput.addEventListener('keyup', input_color, false);
 
-    containerWidth = $(window).width();
-    containerHeight = $(window).height();
-
-    $(".content>section").css({
-        minHeight: containerHeight
-    });
-    
-    console.log(containerHeight);
     initRing();
 }
 
 $(document).ready(function(){
-          
-    $(document).on('click', 'a[href*="#"]', function() {
-        var slashedHash = '#/' + this.hash.slice(1);
-        if ( this.hash ) {
 
-            if ( slashedHash === location.hash ) {
-                $.smoothScroll({scrollTarget: this.hash});
-            } else {
-                $.bbq.pushState(slashedHash);
-            }
+    var options = {
+      $menu: $('.nav_list'),
+      menuSelector: 'a',
+      panelSelector: 'section',
+      namespace: '.panelSnap',
+      onSnapStart: function(){},
+      onSnapFinish: function(){},
+      onActivate: function(){},
+      directionThreshold: 100,
+      slideSpeed: 300
+    };
 
-            return false;
-        }
-    });
+    $('.content').panelSnap(options); 
 
-    $(window).bind('hashchange', function(event) {
-        var tgt = location.hash.replace(/^#\/?/,'');
-        if ( document.getElementById(tgt) ) {
-          $.smoothScroll({scrollTarget: '#' + tgt});
-        }
-    });
+    // $(document).on('click', 'a[href*="#"]', function() {
+    //     var slashedHash = '#/' + this.hash.slice(1);
+    //     if ( this.hash ) {
 
-    $(window).trigger('hashchange');
+    //         if ( slashedHash === location.hash ) {
+    //             $.smoothScroll({scrollTarget: this.hash});
+    //         } else {
+    //             $.bbq.pushState(slashedHash);
+    //         }
+
+    //         return false;
+    //     }
+    // });
+
+    // $(window).bind('hashchange', function(event) {
+    //     var tgt = location.hash.replace(/^#\/?/,'');
+    //     if ( document.getElementById(tgt) ) {
+    //       $.smoothScroll({scrollTarget: '#' + tgt});
+    //     }
+    // });
+
+    // $(window).trigger('hashchange');
 });
 
