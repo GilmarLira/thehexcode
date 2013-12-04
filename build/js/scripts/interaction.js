8b58641e41fd5a548d0a0101bf730319ec9ebb63
Gilmar Lira <gilmar.lira@me.com>
2013-12-04 15:22:32 -0800

    Adds easing to portrait's hover transition effect.
    - HTML changed to place gif on top of jpg portraits, and other refinements
    
    - CSS rewritten using SASS syntax
    
    - Portrait images are now absolute positioned
    
    - Using bourbon's ease-out-quad timming function
    
    - Javascript cleaned-up a bit

--- build/js/scripts/interaction.js
+++ build/js/scripts/interaction.js
@@ -30,8 +30,6 @@ function initContainer() {
     // $(".content>section").css({
     //     minHeight: containerHeight
     // });
-    
-    // console.log(containerHeight);
 
     colorInput = document.getElementById('color-input');
     colorInput.addEventListener('keyup', input_color, false);
@@ -54,28 +52,5 @@ $(document).ready(function(){
     };
 
     $('.content').panelSnap(options); 
-
-    // $(document).on('click', 'a[href*="#"]', function() {
-    //     var slashedHash = '#/' + this.hash.slice(1);
-    //     if ( this.hash ) {
-
-    //         if ( slashedHash === location.hash ) {
-    //             $.smoothScroll({scrollTarget: this.hash});
-    //         } else {
-    //             $.bbq.pushState(slashedHash);
-    //         }
-
-    //         return false;
-    //     }
-    // });
-
-    // $(window).bind('hashchange', function(event) {
-    //     var tgt = location.hash.replace(/^#\/?/,'');
-    //     if ( document.getElementById(tgt) ) {
-    //       $.smoothScroll({scrollTarget: '#' + tgt});
-    //     }
-    // });
-
-    // $(window).trigger('hashchange');
 });
 
