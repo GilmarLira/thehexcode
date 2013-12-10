ce14db6912307ba2b4038e75b2bcfc2dadd3c1fe
Gilmar Lira <gilmar.lira@me.com>
2013-12-09 22:50:14 -0800

    Sets up Display Technology interaction prototype.
    - This is just a prototype, and it is supposed to have rough edges, so don't freak out.

--- build/js/scripts/interaction.js
+++ build/js/scripts/interaction.js
@@ -3,50 +3,31 @@
 // ////////////////////////////////////////////////////
 
 window.addEventListener('load', initContainer, false);
-// document.getElementById("tech-slider").onchange=techSlider(this.value);
-// var techImageContainer = document.getElementById("tech-slider-image-container");
+
 
 function techSlider(value) {
     console.log("Slider value: "+value);
-
     $("#tech-slider-image-container div").hide().eq(value).show();
 }
-
-// var containerWidth, containerHeight, colorInput;
-
-// var delay = (function(){
-//       var timer = 0;
-//       return function(callback, ms){
-//         clearTimeout (timer);
-//         timer = setTimeout(callback, ms);
-//       };
-//     })();
-
-// $(window).resize(function() { 
-//     delay(function(){
-//             console.log('Resize...');
-//             initContainer();
-//         }, 250);
-// });
 
 
 // Get viewport dimensions
 function initContainer() {
-    // containerWidth = $(window).width();
-    // containerHeight = $(window).height();
-
-    // $(".content>section").css({
-    //     minHeight: containerHeight
-    // });
-
     colorInput = document.getElementById('color-input');
     colorInput.addEventListener('keyup', input_color, false);
-
     initRing();
 }
 
-$(document).ready(function(){
 
+// Color depth-interaction
+$(".bit").click(function(){
+  $(".bit-image-container").css('display', 'none').eq($(this).index()).css('display', 'block');
+  $(this).toggleClass("selected", true).siblings(".bit").toggleClass("selected", false);
+
+})
+
+
+$(document).ready(function(){
     var options = {
       $menu: $('.nav_list'),
       menuSelector: 'a',
