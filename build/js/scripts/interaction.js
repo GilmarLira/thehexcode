c8a9e2ad7914f7f36539090ded7821092fc7a56b
Gilmar Lira <gilmar.lira@me.com>
2013-12-11 18:06:52 -0800

    Implements Additive Color interaction prototype.
    - Using SVG shapes to create the interaction
    
    - Overlapping areas are actually separate SVG shapes (7 shapes total), as there is no cleaner way to do this
    
    - Slider controls are standard HTML5 input elements, with just a little bit of styling

--- build/js/scripts/interaction.js
+++ build/js/scripts/interaction.js
@@ -1,52 +1,95 @@
 
 // Interaction
 // ////////////////////////////////////////////////////
-
 window.addEventListener('load', initContainer, false);
+
+// globals
+var sliderR, sliderG, sliderB, sliderL;
+sliderR = 255;
+sliderG = 255;
+sliderB = 255;
+sliderL = 255;
+
 
 // Nav reset
 $(".main_nav li").click(function(){
-  $(".interaction").fadeOut();
+  	$(".interaction").fadeOut();
+});
+
+$(".interaction .close").click(function(){
+  	$(".interaction").fadeOut();
 });
 
 
 // Get viewport dimensions
 function initContainer() {
-    colorInput = document.getElementById('color-input');
-    colorInput.addEventListener('keyup', input_color, false);
-    initRing();
+	colorInput = document.getElementById('color-input');
+	colorInput.addEventListener('keyup', input_color, false);
+	initRing();
 }
 
 
 // Color depth-interaction
-$(".bit").click(function(){
-  $(".bit-image-container").css('display', 'none').eq($(this).index()).css('display', 'block');
-  $(this).toggleClass("selected", true).siblings(".bit").toggleClass("selected", false);
-});
-
 $("#section-technology .interaction-teaser img").click(function(){
-  $("#section-technology .interaction").fadeIn();
+ 	$("#section-technology .interaction").fadeIn();
 });
 
-$(".interaction .close").click(function(){
-  $(".interaction").fadeOut();
+$(".bit").click(function(){
+  	$(".bit-image-container").css('display', 'none').eq($(this).index()).css('display', 'block');
+  	$(this).toggleClass("selected", true).siblings(".bit").toggleClass("selected", false);
 });
+
+
+
+// Additive Color interactions
+$("#section-additivecolor .interaction-teaser > div").click(function(){
+  	$("#section-additivecolor .interaction").fadeIn();
+});
+
+function updateSlider(target){
+  	// console.log(target.id + ": " + target.value);
+
+  	switch(target.id) {
+		case 'r' :
+			sliderR = target.value;
+			// console.log("updated sliderR: "+sliderR);
+			break;
+		case 'g' :
+			sliderG = target.value;
+			// console.log("updated sliderG: "+sliderG);
+			break;
+		case 'b' :
+			sliderB = target.value;
+			// console.log("updated sliderB: "+sliderB);
+			break;
+	}
+
+	$("#circle-r").css('fill','rgb('+sliderR+',0,0)');
+	$("#circle-g").css('fill','rgb(0,'+sliderG+',0)');
+	$("#circle-b").css('fill','rgb(0,0,'+sliderB+')');
+
+	$("#petal-c").css('fill','rgb(0,'+sliderG+','+sliderB+')');
+	$("#petal-m").css('fill','rgb('+sliderR+',0,'+sliderB+')');
+	$("#petal-y").css('fill','rgb('+sliderR+','+sliderG+',0)');
+
+	$("#core").css('fill','rgb('+sliderR+','+sliderG+','+sliderB+')');
+}
 
 
 
 $(document).ready(function(){
-    var options = {
-      $menu: $('.nav_list'),
-      menuSelector: 'a',
-      panelSelector: 'section',
-      namespace: '.panelSnap',
-      onSnapStart: function(){},
-      onSnapFinish: function(){},
-      onActivate: function(){},
-      directionThreshold: 100,
-      slideSpeed: 300
-    };
+	var options = {
+	  	$menu: $('.nav_list'),
+		menuSelector: 'a',
+		panelSelector: 'section',
+		namespace: '.panelSnap',
+		onSnapStart: function(){},
+		onSnapFinish: function(){},
+		onActivate: function(){},
+		directionThreshold: 100,
+		slideSpeed: 300
+	};
 
-    $('.content').panelSnap(options); 
+	$('.content').panelSnap(options); 
 });
 
